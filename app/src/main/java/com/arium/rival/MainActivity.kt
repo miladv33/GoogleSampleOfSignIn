db3eb31375c9bf3c407dd37c02b356fec7f9e91a
Milad <milad.mosavi.developer@gmail.com>
2023-01-13 20:26:52 +0330

    updated to 1.0.2

--- app/src/main/java/com/arium/rival/MainActivity.kt
+++ app/src/main/java/com/arium/rival/MainActivity.kt
@@ -5,17 +5,21 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.googlesignin.GoogleSignIn
-import com.example.googlesignin.listener.IGetIDToken
+import com.example.googlesignin.listener.ISignInResult
+import com.example.googlesignin.models.SignInResult
 
-class MainActivity : AppCompatActivity(), IGetIDToken {
+class MainActivity : AppCompatActivity(), ISignInResult {
 
 
     var googleSignIn: GoogleSignIn? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val googleClientId = ""
+        // define this variable in your localProperties
+        val googleClientId = BuildConfig.googleApiClientID
+
         googleSignIn = GoogleSignIn(this, googleClientId, this)
+        googleSignIn?.init()
         googleSignIn?.signIn()
     }
 
@@ -24,11 +28,21 @@ class MainActivity : AppCompatActivity(), IGetIDToken {
         googleSignIn?.onActivityResult(requestCode, data)
     }
 
-    override fun onGetIdToken(idToken: String) {
-        Toast.makeText(this, idToken, Toast.LENGTH_LONG).show()
+    override fun onGetTheSignInResult(signInResult: SignInResult?) {
+        Toast.makeText(this, signInResult?.displayName, Toast.LENGTH_LONG).show()
     }
 
     override fun onGotAnException(exception: Exception) {
         Toast.makeText(this, exception.message, Toast.LENGTH_LONG).show()
     }
+
+    override fun onTheCanceledExceptionHappened() {
+        Toast.makeText(this, "onTheCanceledExceptionHappened", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onNothingHappened() {
+        Toast.makeText(this, "Nothing happened", Toast.LENGTH_LONG).show()
+    }
+
+
 }
